26fb179751a9d48d1f99b220818efdd763e38ef2
PoPuGaLiC <64127778+PoPuGaLiC@users.noreply.github.com>
2025-01-14 20:42:37 +0500

    Add carousel fix

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -7,58 +7,91 @@ import Link from "next/link";
 export default function Carousel() {
   return (
 
-    <div id="horizontal-thumbnails" data-carousel='{ "loadingClasses": "opacity-0" }' className="relative w-full">
-      <div className="carousel">
-        <div className="carousel-body h-1/4 opacity-0">
-          <div className="carousel-slide">
-            <div className="flex size-full justify-center">
+    <div id="horizontal-thumbnails" data-carousel='{ "loadingClasses": "opacity-0" }' className=" h-[600px] relative w-full lg:xl:px-[5%]  xl:2xl:px-[10%] 2xl:max-[6000px]:px-[15%]">
+      <div className="carousel size-full ">
+        <div className="carousel-body opacity-0  h-3/4">
+          <div className="carousel-slide ">
+            <div className="flex size-full  justify-center">
               <Image
-                className="size-full object-cover"
+                className="size-full object-cover object-center"
                 aria-hidden
                 src="/carousel/20220519_142952.jpg"
                 alt="File icon"
-                width={160}
-                height={160}
+                width={2016}
+                height={1512}
               />
-              {/* <img
-                src="https://cdn.flyonui.com/fy-assets/components/carousel/image-21.png"
-                className="size-full object-cover"
-                alt="mountain"
-              /> */}
             </div>
           </div>
           <div className="carousel-slide">
             <div className="flex size-full justify-center">
               <Image
-                className="size-full object-cover"
+                className="size-full object-cover object-center"
                 aria-hidden
                 src="/carousel/20220625_090350.jpg"
                 alt="File icon"
-                width={160}
-                height={160}
+                width={2016}
+                height={1512}
               />
-              {/* <img
-                src="https://cdn.flyonui.com/fy-assets/components/carousel/image-14.png"
-                className="size-full object-cover"
-                alt="sand"
-              /> */}
             </div>
           </div>
           <div className="carousel-slide">
-            <div className="flex size-full justify-center">
+            <div className="flex size-full justify-center object-center">
               <Image
                 className="size-full object-cover"
                 aria-hidden
                 src="/carousel/20220625_090408.jpg"
                 alt="File icon"
-                width={160}
-                height={160}
+                width={2016}
+                height={1512}
               />
-              {/* <img
-                src="https://cdn.flyonui.com/fy-assets/components/carousel/image-7.png"
+            </div>
+          </div>
+          <div className="carousel-slide ">
+            <div className="flex size-full  justify-center object-center">
+              <Image
                 className="size-full object-cover"
-                alt="cloud"
-              /> */}
+                aria-hidden
+                src="/carousel/20220625_090420.jpg"
+                alt="File icon"
+                width={2016}
+                height={1512}
+              />
+            </div>
+          </div>
+          <div className="carousel-slide ">
+            <div className="flex size-full  justify-center object-center">
+              <Image
+                className="size-full object-cover"
+                aria-hidden
+                src="/carousel/20220625_090852.jpg"
+                alt="File icon"
+                width={2016}
+                height={1512}
+              />
+            </div>
+          </div>
+          <div className="carousel-slide ">
+            <div className="flex size-full  justify-center object-center">
+              <Image
+                className="size-full object-cover"
+                aria-hidden
+                src="/carousel/20230516_155649.jpg"
+                alt="File icon"
+                width={2016}
+                height={1512}
+              />
+            </div>
+          </div>
+          <div className="carousel-slide ">
+            <div className="flex size-full  justify-center object-center">
+              <Image
+                className="size-full object-cover"
+                aria-hidden
+                src="/carousel/20230517_143207.jpg"
+                alt="File icon"
+                width={2016}
+                height={1512}
+              />
             </div>
           </div>
         </div>
@@ -87,9 +120,38 @@ export default function Carousel() {
             width={160}
             height={160}
           />
-          {/* <img src="https://cdn.flyonui.com/fy-assets/components/carousel/image-21.png" className="carousel-pagination-item carousel-active:opacity-100 grow object-cover opacity-30" alt="mountain" />
-          <img src="https://cdn.flyonui.com/fy-assets/components/carousel/image-14.png" className="carousel-pagination-item carousel-active:opacity-100 grow object-cover opacity-30" alt="sand" />
-          <img src="https://cdn.flyonui.com/fy-assets/components/carousel/image-7.png" className="carousel-pagination-item carousel-active:opacity-100 grow object-cover opacity-30" alt="cloud" /> */}
+          <Image
+            className="carousel-pagination-item carousel-active:opacity-100 grow object-cover opacity-30"
+            aria-hidden
+            src="/carousel/20220625_090420.jpg"
+            alt="File icon"
+            width={160}
+            height={160}
+          />
+          <Image
+            className="carousel-pagination-item carousel-active:opacity-100 grow object-cover opacity-30"
+            aria-hidden
+            src="/carousel/20220625_090852.jpg"
+            alt="File icon"
+            width={160}
+            height={160}
+          />
+          <Image
+            className="carousel-pagination-item carousel-active:opacity-100 grow object-cover opacity-30"
+            aria-hidden
+            src="/carousel/20230516_155649.jpg"
+            alt="File icon"
+            width={160}
+            height={160}
+          />
+          <Image
+            className="carousel-pagination-item carousel-active:opacity-100 grow object-cover opacity-30"
+            aria-hidden
+            src="/carousel/20230517_143207.jpg"
+            alt="File icon"
+            width={160}
+            height={160}
+          />
         </div>
         <button type="button" className="carousel-prev">
           <span className="mb-15" aria-hidden="true">
@@ -109,6 +171,7 @@ export default function Carousel() {
         </button>
       </div>
     </div>
+
 
   );
 }
